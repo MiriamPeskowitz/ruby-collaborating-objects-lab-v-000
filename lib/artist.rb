fba987458b3f7108c12372ad2d6600472e4314ca
Miriam Peskowitz <miriam5000@gmail.com>
2017-12-13 16:33:58 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,96 +1,96 @@
 
-# class Artist
-#   @@all = []
-#   attr_accessor :name, :songs
-#
-#   def initialize(name)
-#     @name = name
-#     @songs = []
-#   end
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def add_song(song)
-#     @songs << song
-#   end
-#
-#   def self.find_or_create_by_name(name)
-#     self.find(name) ? self.find(name) : self.create(name)
-#   end
-#
-#   def self.find(name)
-#     self.all.find {|artist| artist.name == name }
-#   end
-#
-#
-#   def self.create(name)
-#     self.new(name).tap {|artist| artist.save}
-#   end
-#
-#   def save
-#     @@all << self
-#   end
-#
-#   def print_songs
-#     songs.each {|song| puts song.name}
-#   end
-# end
-#
-
-require 'pry'
-
 class Artist
-  attr_accessor :name, :songs
   @@all = []
+  attr_accessor :name, :songs
 
-    def initialize(name)
-      @name = name
-      @songs = []
-    end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-    def songs
-      @songs
-    end
+  def self.all
+    @@all
+  end
 
-    def add_song(song)
-      song.artist = self
-      @songs << song
-    end
+  def add_song(song)
+    @songs << song
+  end
 
-    def save
-      @@all << self
-    end
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
+  end
 
-    def self.all
-      @@all
-    end
-
-    def self.find_or_create_by_name(name)
-      self.find(name) ? self.find(name) : name = Artist.new(name)
-
-      # self.find(name) {|artist| artist == name }
-      #   if artist == name
-      #     self.name
-      #   else
-      #     add_song_by_name(name)
-      #   end
-    end
-
-    def self.find(name)
-      self.all.find {|a| a.name == name}
-    end
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
 
 
-    def add_song_by_name(name)
-      song = Song.new(name)
-      song.artist = self
-      @songs<<song
-    end
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
+  end
 
-    def print_songs(name)
-      binding.pry
-      @songs.each {|song| puts song.name}
-    end
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
 end
+
+# 
+# require 'pry'
+# 
+# class Artist
+#   attr_accessor :name, :songs
+#   @@all = []
+# 
+#     def initialize(name)
+#       @name = name
+#       @songs = []
+#     end
+# 
+#     def songs
+#       @songs
+#     end
+# 
+#     def add_song(song)
+#       song.artist = self
+#       @songs << song
+#     end
+# 
+#     def save
+#       @@all << self
+#     end
+# 
+#     def self.all
+#       @@all
+#     end
+# 
+#     def self.find_or_create_by_name(name)
+#       self.find(name) ? self.find(name) : name = Artist.new(name)
+# 
+#       # self.find(name) {|artist| artist == name }
+#       #   if artist == name
+#       #     self.name
+#       #   else
+#       #     add_song_by_name(name)
+#       #   end
+#     end
+# 
+#     def self.find(name)
+#       self.all.find {|a| a.name == name}
+#     end
+# 
+# 
+#     def add_song_by_name(name)
+#       song = Song.new(name)
+#       song.artist = self
+#       @songs<<song
+#     end
+# 
+#     def print_songs(name)
+#       binding.pry
+#       @songs.each {|song| puts song.name}
+#     end
+# end
