926f928a43895135b824e00a29da002ef6d330f2
Miriam Peskowitz <miriam5000@gmail.com>
2017-12-13 16:18:09 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,48 +2,48 @@
 # class Artist
 #   @@all = []
 #   attr_accessor :name, :songs
-# 
+#
 #   def initialize(name)
 #     @name = name
 #     @songs = []
 #   end
-# 
+#
 #   def self.all
-#     @@all    
+#     @@all
 #   end
-# 
+#
 #   def add_song(song)
 #     @songs << song
 #   end
-# 
+#
 #   def self.find_or_create_by_name(name)
 #     self.find(name) ? self.find(name) : self.create(name)
 #   end
-# 
+#
 #   def self.find(name)
 #     self.all.find {|artist| artist.name == name }
 #   end
-# 
-# 
+#
+#
 #   def self.create(name)
 #     self.new(name).tap {|artist| artist.save}
 #   end
-# 
+#
 #   def save
 #     @@all << self
 #   end
-# 
+#
 #   def print_songs
 #     songs.each {|song| puts song.name}
 #   end
 # end
-# 
+#
 
 require 'pry'
 
 class Artist
 #if this artist is not in the array, create the artist, else send string of the artist's name to the Artist class,
-attr_accessor :name
+attr_accessor :name, :songs
 @@all = []
 
     def initialize(name)
@@ -91,6 +91,7 @@ attr_accessor :name
     end
 
     def print_songs(name)
+      binding.pry
       @songs.map {|song| puts song.name}
     end
 end
