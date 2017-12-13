918971b2959d9f314040c7d9d2c6326b7f9096b5
Miriam Peskowitz <miriam5000@gmail.com>
2017-12-13 17:03:51 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -18,10 +18,31 @@
 #     binding.pry
 #     files.each do|f|
 #       Song.new_by_filename(f)
-#       Artist.find_or_create_by_name(f)
+# #       Artist.find_or_create_by_name(f)
+# #     end
+# #   end
+# # end
+# class MP3Importer
+#   attr_reader :path
+# 
+#   def initialize(path)
+#     @path = path
+#   end
+# 
+#   def files
+#     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+#   end
+# 
+#   def import(song)
+#     # files.map{|f| Song.new_by_filename(f)}
+# 
+#     files.each do |f|
+#       Song.new_by_filename(f)
 #     end
 #   end
 # end
+# 
+
 class MP3Importer
   attr_reader :path
 
@@ -33,29 +54,7 @@ class MP3Importer
     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 
-  def import(path)
-    # files.map{|f| Song.new_by_filename(f)}
-
-    files.each do |f|
-        binding.pry
-      Song.new_by_filename(f)
-    end
+  def import
+    files.each{|f| Song.new_by_filename(f)}
   end
 end
-
-
-# class MP3Importer
-#   attr_reader :path
-#
-#   def initialize(path)
-#     @path = path
-#   end
-#
-#   def files
-#     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
-#   end
-#
-#   def import
-#     files.each{|f| Song.new_by_filename(f)}
-#   end
-# end
