d8ba50a4150574903b02f505049d8b07b8b76d49
Miriam Peskowitz <miriam5000@gmail.com>
2017-12-13 17:03:56 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -24,24 +24,24 @@
 # # end
 # class MP3Importer
 #   attr_reader :path
-# 
+#
 #   def initialize(path)
 #     @path = path
 #   end
-# 
+#
 #   def files
 #     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
 #   end
-# 
+#
 #   def import(song)
 #     # files.map{|f| Song.new_by_filename(f)}
-# 
+#
 #     files.each do |f|
 #       Song.new_by_filename(f)
 #     end
 #   end
 # end
-# 
+#
 
 class MP3Importer
   attr_reader :path
