1a2af894fe7fd1b80771aea78b3519eac5bacb20
Miriam Peskowitz <miriam5000@gmail.com>
2017-12-13 16:16:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,46 @@
-require 'pry'
-class Song
-  attr_accessor :artist, :name
-  @@all = []
 
-  def initialize(name, artist)
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
     @name = name
-    @artist = artist
   end
 
-  def self.new_by_filename(filename) #figure out what the filename format is, and use split or gsub.
-    # binding.pry
+  def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
-    new_song.artist_name= artist
+    new_song.artist_name = artist
     new_song
   end
 
   def artist_name=(name)
-     @name = name
-     self.artist= Artist.find_or_create_by_name(name)
-     artist.add_song(self)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 end
+
+
+# require 'pry'
+# class Song
+#   attr_accessor :artist, :name
+#   @@all = []
+#
+#   def initialize(name, artist)
+#     @name = name
+#     @artist = artist
+#   end
+#
+#   def self.new_by_filename(filename) #figure out what the filename format is, and use split or gsub.
+#     # binding.pry
+#     artist, song = filename.split(" - ")
+#     new_song = self.new(song)
+#     new_song.artist_name= artist
+#     new_song
+#   end
+#
+#   def artist_name=(name)
+#      @name = name
+#      self.artist= Artist.find_or_create_by_name(name)
+#      artist.add_song(self)
+#   end
+# end
